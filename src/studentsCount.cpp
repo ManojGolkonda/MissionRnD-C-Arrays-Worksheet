7c0af7ebea0c525b4544e6fafeadf40cdc06b5f4
mayago <golkondamanoj@gmail.com>
2015-11-11 09:13:41 +0530

    Submit Commit

--- src/studentsCount.cpp
+++ src/studentsCount.cpp
@@ -16,5 +16,35 @@ NOTES:
 #include <stdio.h>
 
 void * studentsCount(int *Arr, int len, int score, int *lessCount, int *moreCount) {
-	return NULL;
+	if (Arr == NULL || len<1)			//handling for invalid cases
+		return NULL;
+	int i = 0, j = len - 1, m;
+	/*	while (i < j)						//finding the index of the given score, so that we can find numbers greater and lesser than the given score
+	{									//this is same as binary search :)
+	m = (i + j) / 2;
+	if (Arr[m]>score)
+	j = m;
+	else if (Arr[m] < score)
+	i = m;
+	else
+	{
+	i = m;
+	j = m;
+	break;
+	}
+	}
+	while (i > 0 && Arr[i - 1] == score)			//handling duplicates before the number we found
+	i--;
+	while (j < len - 2 && Arr[j + 1] == score)		//handling duplicates after the number we found
+	j++;
+	*/
+	while (Arr[i] < score && i < len)
+		i++;
+	j = i;
+	while (Arr[j] == score && j < len)
+		j++;
+	/**lessCount = i;									//updating result
+	*moreCount = len - j - 1;*/
+	*lessCount = i;
+	*moreCount = len - j;
 }
